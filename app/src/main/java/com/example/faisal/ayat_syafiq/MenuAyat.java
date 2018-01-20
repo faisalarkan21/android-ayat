275e99ed0fd97b13c2d737b72a9069774cb50007
faisal arkan <faisalarkan21@gmail.com>
2018-01-21 02:02:44 +0700

    fix layout ver 0.1

--- app/src/main/java/com/example/faisal/ayat_syafiq/MenuAyat.java
+++ app/src/main/java/com/example/faisal/ayat_syafiq/MenuAyat.java
@@ -47,10 +47,13 @@ public class MenuAyat extends AppCompatActivity {
         ayatJadi = databaseAccess.getDataQuranByAyatTest(listAyatArray, listSuratArray);
         databaseAccess.close();
 
+
         List<TextView> textListNama = new ArrayList<TextView>( listSuratArray.size());
         List<TextView> textListAyat = new ArrayList<TextView>( listSuratArray.size());
         List<TextView> textListArti = new ArrayList<TextView>( listSuratArray.size());
         List<TextView> textListKeterangan = new ArrayList<TextView>( listSuratArray.size());
+        List<TextView> textListLabelArti = new ArrayList<TextView>( listSuratArray.size());
+        List<TextView> textListLabelKeterangan = new ArrayList<TextView>( listSuratArray.size());
 
         for(int i = 0; i < ayatJadi.getListAyat().size(); i++)
         {
@@ -60,8 +63,9 @@ public class MenuAyat extends AppCompatActivity {
 
             TextView newNama = new TextView(MenuAyat.this);
             newNama.setText(listNamaArray.get(i).toString());
-            newNama.setPadding(20,20,20,20);
-            newNama.setTextSize(25);
+            newNama.setPadding(50,50,20,20);
+            newNama.setTextColor(Color.parseColor("#000000"));
+            newNama.setTextSize(20);
 
             /*
                 Text Ayat Arab
@@ -70,27 +74,45 @@ public class MenuAyat extends AppCompatActivity {
             TextView newAyat = new TextView(MenuAyat.this);
             newAyat.setText(ayatJadi.getListAyat().get(i).toString());
             newAyat.setTextColor(Color.parseColor("#000000"));
-            newAyat.setPadding(20,20,20,20);
-
+            newAyat.setPadding(20,40,50,20);
+            newAyat.setLineSpacing(0,1.6f);
             newAyat.setTextSize(25);
 
              /*
                 Text Ayat Arti
              */
 
+            TextView newArtiLabel = new TextView(MenuAyat.this);
+            newArtiLabel.setText("Artinya : ");
+            newArtiLabel.setPadding(50,60,20,0);
+            newArtiLabel.setTextColor(Color.parseColor("#000000"));
+            newArtiLabel.setLineSpacing(0,1.5f);
+            newArtiLabel.setTextSize(13);
+
+
             TextView newArti = new TextView(MenuAyat.this);
             newArti.setText(ayatJadi.getListArti().get(i).toString().replace("&quot;", ""));
-            newArti.setPadding(20,20,20,20);
-            newArti.setTextSize(12);
+            newArti.setPadding(50,20,20,20);
+            newArti.setTextColor(Color.parseColor("#000000"));
+            newArti.setLineSpacing(0,1.5f);
+            newArti.setTextSize(15);
+
+            TextView newKeteranganLabel = new TextView(MenuAyat.this);
+            newKeteranganLabel.setText("Keterangan : ");
+            newKeteranganLabel.setPadding(50,30,20,0);
+            newKeteranganLabel.setTextColor(Color.parseColor("#000000"));
+            newKeteranganLabel.setLineSpacing(0,1.5f);
+            newKeteranganLabel.setTextSize(13);
 
              /*
                 Text Ayat Keterangan
              */
 
             TextView newKeterangan = new TextView(MenuAyat.this);
-            newKeterangan.setText("* " + listKeterangan.get(i).toString().replace("&quot;", ""));
-            newKeterangan.setPadding(20,20,20,20);
-            newKeterangan.setTextSize(12);
+            newKeterangan.setText(listKeterangan.get(i).toString().replace("&quot;", ""));
+            newKeterangan.setPadding(50,20,20,20);
+            newKeterangan.setTextColor(Color.parseColor("#000000"));
+            newKeterangan.setTextSize(14);
 
             // Tambah nama
             textListNama.add(newNama);
@@ -100,13 +122,22 @@ public class MenuAyat extends AppCompatActivity {
             layout.addView(newAyat);
             textListAyat.add(newAyat);
 
+            layout.addView(newArtiLabel);
+            textListLabelArti.add(newArtiLabel);
+
             // Tambah Arti
             layout.addView(newArti);
             textListArti.add(newArti);
 
+
+            layout.addView(newKeteranganLabel);
+            textListLabelKeterangan.add(newKeteranganLabel);
+
             // Tambah Keterangan
             layout.addView(newKeterangan);
             textListKeterangan.add(newKeterangan);
+
+
 
         }
     }
